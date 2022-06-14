ae2c59f3bf944dc6461faa9301b8b5af46833437
akhilthala <thalamalaakhil@gmail.com>
2022-06-14 10:14:35 +0530

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :restaurants
+root "restaurants#index"
+  get 'pages/contact'
+  get 'pages/about'
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
